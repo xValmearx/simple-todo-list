c608473c6933ebde1f1eaf6831422060a0c4dfc2
Caleb Taylor <calebs.phone2017@gmail.com>
2024-09-19 20:30:48 -0400

    added an admin user to the django project

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    """Post model"""
+
+    """text is the column in the data base"""
+    text = models.TextField()
